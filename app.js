bdb05611f470605e91f76184a9d174d34b21a15c
Ashton Foley <foleyash@umich.edu>
2023-02-21 12:04:56 -0500

    First commit

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
-const startGame = document.querySelector(".start-text");
-var start = false;
-startGame.addEventListener("click", () => {
-    start = true;
-})
+var play = document.getElementById("play_button");
+var start = document.querySelector(".start");
+var hover = false;
 
-if(start == true) {
-    
+var interval = window.setInterval(function(){
+    if(hover == true) {
+        return;
+    }
+    if(play.style.opacity == .01) {
+        play.style.opacity = 1;
+    }
+    else {
+        play.style.opacity = .01;
+    }
+}, 500);
+
+play.onmouseover = function() {
+    play.style.color = 'white';
+    play.style.scale = 1.1;
+    play.style.opacity = 1;
+    hover = true;
+};
+
+play.onmouseout = function() {
+    play.style.color = 'blue';
+    play.style.scale = 1;
+    hover = false;
 }
+
+play.addEventListener('click', (event) => {
+    play.style.visibility = hidden;
+    start.style.visibility = hidden;
+});
+
